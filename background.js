962639fddbde160464bcd450023b39175d2ed445
Sampath Kumar Krishnan <sampath3493@gmail.com>
2020-06-06 15:57:50 +0530

    fix: Issue in updating mic state when mic is clicked in meet - chrome.tabs.onUpdated is called when a tab is updated in chrome - For meet, when the user joins a call, chrome.tabs.onUpdated is called with the changeInfo as { 'audible': 'false' } - Using that as a trigger send a message to content script, to start listening for click events

--- background.js
+++ background.js
@@ -19,6 +19,23 @@ chrome.commands.onCommand.addListener(function (command) {
   toggleMicrophone();
 });
 
+// chrome.tabs.onUpdated is called when a tab is updated in chrome
+// For meet, when the user joins a call, chrome.tabs.onUpdated is called with the changeInfo as 
+// { 'audible': 'false' }
+// Using that as a trigger send a message to content script, to start listening for click events
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  console.log('Tab updated', tabId, changeInfo, tab);
+  if(changeInfo.audible === false){
+    chrome.tabs.query({ url: "https://meet.google.com/*" }, function (tabs) {
+    if (tabs && tabs.length > 0) {
+      const firstMeetTab = tabs[0];
+      console.log(firstMeetTab);
+      chrome.tabs.sendMessage(firstMeetTab.id, { listenToMicClick: true });
+    }
+  });
+  }
+})
+
 chrome.runtime.onMessage.addListener(function (request, sender) {
   console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
   if (request.micMuted) {
--- togglemic.js
+++ togglemic.js
@@ -1,21 +1,23 @@
-let micButton;
+let micButton = null;
 
 window.addEventListener(
   "load",
   function () {
-    micButton = document.querySelector(".sUZ4id").children[0];
-    micButton.addEventListener("click", (e) => {
-      this.setTimeout(() => {
-        const micMuted = micButton.getAttribute("data-is-muted");
-        console.log("Mic-muted: ", micMuted);
-        chrome.runtime.sendMessage({ micMuted: micMuted === "true" ? "OFF" : "ON" });
-      }, 0);
-    });
-
+    console.log('window load called');
     chrome.runtime.onMessage.addListener(function (request, sender) {
       console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
       if (request.toggleMic) {
+        // const micButton = document.querySelector(".sUZ4id").children[0];
         micButton.click();
+      } else if (request.listenToMicClick){
+        micButton = document.querySelector(".sUZ4id").children[0];
+        micButton.addEventListener("click", (e) => {
+          this.setTimeout(() => {
+            const micMuted = micButton.getAttribute("data-is-muted");
+            console.log("Mic-muted: ", micMuted);
+            chrome.runtime.sendMessage({ micMuted: micMuted === "true" ? "OFF" : "ON" });
+          }, 0);
+        });    
       }
       return true;
     });
