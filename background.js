d9b7bc6edf5eb1fb53c5bc5b24b13f5306b47195
Mahadevan Sreenivasan <mahadevan_sv@yahoo.com>
2020-06-06 10:56:31 +0530

    feat: Message Passing b/w background & content-script - update manifest.json to change hot key to Ctrl+Shift+Q - cleanup logs in background.js - update togglemic.js to load the script only on window.load by setting a listener - update state of microphone in a dummy setInterval as there is a delay in updating the click event when toggling the mute button programmatically.

--- background.js
+++ background.js
@@ -1,25 +1,24 @@
 chrome.runtime.onInstalled.addListener(function () {
-  console.log("hello");
+  console.log("Google Meet PTT Installed");
+});
 
-  chrome.commands.onCommand.addListener(function (command) {
-    console.log("Command:", command);
-    chrome.tabs.query({ url: "https://meet.google.com/*" }, function (tabs) {
-      const firstMeetTab = tabs[0];
-      console.log(firstMeetTab);
-      chrome.tabs.sendMessage(firstMeetTab.id, { toggleMic: true });
+chrome.commands.onCommand.addListener(function (command) {
+  console.log("Command:", command);
+  chrome.tabs.query({ url: "https://meet.google.com/*" }, function (tabs) {
+    const firstMeetTab = tabs[0];
+    console.log(firstMeetTab);
+    chrome.tabs.sendMessage(firstMeetTab.id, { toggleMic: true });
+  });
+});
+
+chrome.runtime.onMessage.addListener(function (request, sender) {
+  console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
+  if (request.micMuted) {
+    chrome.notifications.create({
+      title: "Google Meet PTT",
+      type: "basic",
+      iconUrl: "mute-icon.png",
+      message: `Mic is ${request.micMuted}`,
     });
-  });
-
-  chrome.runtime.onMessage.addListener(function (request, sender) {
-    console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
-    console.log(request);
-    if (request.micMuted) {
-      chrome.notifications.create({
-        title: "Google Meet PTT",
-        type: "basic",
-        iconUrl: "mute-icon.png",
-        message: `Mic is ${request.micMuted}`,
-      });
-    }
-  });
+  }
 });
--- togglemic.js
+++ togglemic.js
@@ -1,17 +1,24 @@
 let micButton;
 
-window.onload = function () {
-  micButton = document.querySelector(".sUZ4id").children[0];
-  micButton.addEventListener("click", (e) => {
-    const micMuted = micButton.getAttribute("data-is-muted");
-    chrome.runtime.sendMessage({ micMuted });
-  });
-};
+window.addEventListener(
+  "load",
+  function () {
+    micButton = document.querySelector(".sUZ4id").children[0];
+    micButton.addEventListener("click", (e) => {
+      this.setTimeout(() => {
+        const micMuted = micButton.getAttribute("data-is-muted");
+        console.log("Mic-muted: ", micMuted);
+        chrome.runtime.sendMessage({ micMuted: micMuted === "true" ? "OFF" : "ON" });
+      }, 0);
+    });
 
-chrome.runtime.onMessage.addListener(function (request, sender) {
-  console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
-  if (request.toggleMic) {
-    micButton.click();
-  }
-  return true;
-});
+    chrome.runtime.onMessage.addListener(function (request, sender) {
+      console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
+      if (request.toggleMic) {
+        micButton.click();
+      }
+      return true;
+    });
+  },
+  false
+);
