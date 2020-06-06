9279f9a81f530555e164960fc6f8b6e532bc2446
Mahadevan Sreenivasan <mahadevan_sv@yahoo.com>
2020-06-06 12:11:36 +0530

    feat: Clicking on extension icon toggles microphone 1. background.js - Move microphone toggle to a separate function - listen for extension icon click event and toggle microphone - listen for chrome tab removal event and revert to default icon (grey) when no more meet tabs exist 2. manifest.json - add browser_action to set default_icon (grey). This is temporary and needs change - browser_action is required for clicking on the extension icon 3. Add temporary default grey icon when no meet tabs are active (38x38) 4. Add mic-ON and mic-OFF as 38x38 pixels as these are required for browser_action icons.

--- background.js
+++ background.js
@@ -2,13 +2,21 @@ chrome.runtime.onInstalled.addListener(function () {
   console.log("Google Meet PTT Installed");
 });
 
+const toggleMicrophone = function () {
+  chrome.tabs.query({ url: "https://meet.google.com/*" }, function (tabs) {
+    if (tabs && tabs.length > 0) {
+      const firstMeetTab = tabs[0];
+      console.log(firstMeetTab);
+      chrome.tabs.sendMessage(firstMeetTab.id, { toggleMic: true });
+    } else {
+      chrome.browserAction.setIcon({ path: `/mic-default-38x38.png` });
+    }
+  });
+};
+
 chrome.commands.onCommand.addListener(function (command) {
   console.log("Command:", command);
-  chrome.tabs.query({ url: "https://meet.google.com/*" }, function (tabs) {
-    const firstMeetTab = tabs[0];
-    console.log(firstMeetTab);
-    chrome.tabs.sendMessage(firstMeetTab.id, { toggleMic: true });
-  });
+  toggleMicrophone();
 });
 
 chrome.runtime.onMessage.addListener(function (request, sender) {
@@ -20,5 +28,19 @@ chrome.runtime.onMessage.addListener(function (request, sender) {
       iconUrl: `mic-${request.micMuted}.png`,
       message: `Mic is ${request.micMuted}`,
     });
+    //update extension icon
+    chrome.browserAction.setIcon({ path: `/mic-${request.micMuted}-38x38.png` });
   }
+});
+
+chrome.browserAction.onClicked.addListener(function callback() {
+  toggleMicrophone();
+});
+
+chrome.tabs.onRemoved.addListener(function callback() {
+  chrome.tabs.query({ url: "https://meet.google.com/*" }, function (tabs) {
+    if (!tabs || tabs.length == 0) {
+      chrome.browserAction.setIcon({ path: `/mic-default-38x38.png` });
+    }
+  });
 });
