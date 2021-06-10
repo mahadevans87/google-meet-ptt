900a579cd77259a4e3980361f4cc7e162fefa8b4
Sampath Kumar Krishnan <sampathBlam@users.noreply.github.com>
2021-06-10 19:26:14 +0530

    Add check to identify the correct mic button element - Identify mic button element based on the availability of 'data-is-muted' attribute

--- togglemic.js
+++ togglemic.js
@@ -1,9 +1,13 @@
 let micButton;
 
 function handleMicClick(event) {
+  let micMuted;
   setTimeout(function () {
-    micMuted = document.querySelector(".VfPpkd-Bz112c-LgbsSe").getAttribute("data-is-muted");
-    // console.log("Mic-muted: ", micMuted);
+    if(document.querySelectorAll('.GKGgdd')[0].children[0].getAttribute('data-is-muted') != null){
+      micMuted = document.querySelectorAll('.GKGgdd')[0].children[0].getAttribute('data-is-muted');
+    } else {
+      micMuted = document.querySelectorAll('.GKGgdd')[0].children[0].children[0].getAttribute('data-is-muted');
+    }
     chrome.runtime.sendMessage({ micMuted: micMuted === "true" ? "OFF" : "ON" });
   }, 0);
 }
@@ -15,14 +19,22 @@ window.addEventListener(
     chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
       // console.log("Message", sender.tab ? "from a content script:" + sender.tab.url : "from the extension", request);
       if (request.toggleMic) {
-        micButton = document.querySelector(".VfPpkd-Bz112c-LgbsSe");
+        if(document.querySelectorAll('.GKGgdd')[0].children[0].getAttribute('data-is-muted')){
+          micButton = document.querySelectorAll('.GKGgdd')[0].children[0];
+        } else {
+          micButton = document.querySelectorAll('.GKGgdd')[0].children[0].children[0];
+        }
         micButton.click();
         return true;
       } else if (request.listenToMicClick) {
         // if(micButton != null){
         //   micButton.removeEventListener('click',handleMicClick);
         // }
-        micButton = document.querySelector(".VfPpkd-Bz112c-LgbsSe");
+        if(document.querySelectorAll('.GKGgdd')[0].children[0].getAttribute('data-is-muted')){
+          micButton = document.querySelectorAll('.GKGgdd')[0].children[0];
+        } else {
+          micButton = document.querySelectorAll('.GKGgdd')[0].children[0].children[0];
+        }
         let micMuted = micButton.getAttribute("data-is-muted");
         micButton.addEventListener("click", handleMicClick);
         // Send response back to the extension so that
